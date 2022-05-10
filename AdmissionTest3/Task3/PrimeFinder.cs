5130c4c2df9fd925ae85ad10c0c8af7a314a8096
Md.Hasibul Hossain <mdhasibulhossain8@gmail.com>
2022-05-10 22:51:50 +0600

    adding 3

---
+++ AdmissionTest3/Task3/PrimeFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class PrimeFinder
+    {
+        public int[] GetPrimes(int lowerValue, int upperValue)
+        {
+            int num, i, ctr, j = 0;
+
+            int[] counts = new int[upperValue];
+
+       
+            for (num = lowerValue; num <= upperValue; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                {
+                    
+                   counts[j] = num;
+                    j++;
+                }
+            }
+            return counts;
+        }
+    }
+}
+    
---
+++ AdmissionTest3/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task3;
+
+Console.WriteLine("Hello, World!");
+PrimeFinder primeFinder = new PrimeFinder();
+var actualResult = primeFinder.GetPrimes(1,100);
+
+foreach(var result in actualResult)
+    if(result!=0)
+    Console.Write($"{result}, ");
