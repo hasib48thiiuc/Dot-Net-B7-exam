4c91ecc44ca0550b85f55fd3b9ccff0be270e470
Md.Hasibul Hossain <mdhasibulhossain8@gmail.com>
2022-05-10 23:31:55 +0600

    ad

--- AdmissionTest3/Task1/Program.cs
+++ AdmissionTest3/Task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task1;
+
+string sp = Console.ReadLine();
+
+VowelCounter mass = new VowelCounter();
+
+string final = mass.GetCounts(sp);
+
+
+
+char[] finc = final.ToCharArray();
+int i = 0;
+while( i < finc.Length)
+{
+    if (i == 0)
+
+        Console.Write("a-" + finc[i] + ",");
+    else if (i == 1)
+        Console.Write("e-" + finc[i] + ",");
+    else if (i == 2)
+        Console.Write("i-" + finc[i] + ",");
+    else if (i == 3)
+        Console.Write("o-" + finc[i] + ",");
+    else if (i == 4)
+        Console.Write("u-" + finc[i]);
+    i++;
+}
+
--- AdmissionTest3/Task1/VowelCounter.cs
+++ AdmissionTest3/Task1/VowelCounter.cs
@@ -8,9 +8,36 @@ namespace Task1
 {
     public class VowelCounter
     {
-        public string GetCounts(string text)
+        public string GetCounts(string mess)
         {
-            throw new NotImplementedException();
+            int[] sealer = new int[100] ;
+            int i = 0;
+            while(i < mess.Length)
+            {
+                if (mess[i] == 'a' || mess[i] == 'A')
+                {
+                    sealer[0]++;
+                }
+                else if (mess[i] == 'e' || mess[i] == 'E')
+                {
+                    sealer[1]++;
+                }
+                else if (mess[i] == 'i' || mess[i] == 'I')
+                {
+                    sealer[2]++;
+                }
+                else if (mess[i] == 'o' || mess[i] == 'O')
+                {
+                    sealer[3]++;
+                }
+                else if (mess[i] == 'u' || mess[i] == 'U')
+                {
+                    sealer[4]++;
+                }
+                i++;
+            }
+            string summ = String.Join("", sealer);
+            return summ;
         }
     }
 }
