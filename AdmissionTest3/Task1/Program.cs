296aa968c50b5c202070d1706ef6ffb07c8925b1
Asad-pocket <asadmail119@gmail.com>
2022-05-11 00:16:13 +0600

    .

--- AdmissionTest3/Task1/Program.cs
+++ AdmissionTest3/Task1/Program.cs
@@ -8,10 +8,3 @@ string final = mass.GetCounts(sp);
 
 
 
-char[] finc = final.ToCharArray();
-int i = 0;
-
-       
-
-
-Console.Write("a-" + finc[0] + ","+ "e-" + finc[1] + ","+ "i-"+ finc[2] + ","+ "o-"+ finc[3]+ ","+ "u-" + finc[4] + ",");
